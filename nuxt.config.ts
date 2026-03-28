aadb283b7f8e41eea91650f80bb7d23480e94fe8
robertoneves <robertonevessilva#gmail.comm>
2026-03-28 01:30:21 -0300

    ajustes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   
   // Renderizar apenas no cliente (SPA mode) - necessário para PWA com localStorage
